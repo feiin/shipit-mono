6f70c9364b98aad28a72cc793b5ff9a7d92928ec
feiin <feiin@qq.com>
2016-03-31 15:23:36 +0800

    add default xbuild config

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict'
 var _ = require('lodash');
+var utils = require('shipit-utils');
 
 module.exports = function(shipit) {
     _.assign(shipit.constructor.prototype, require('shipit-deploy/lib/shipit'));
     require("shipit-deploy")(shipit);
     require('./task/deploy/build')(shipit);
 
-    shipit.on('fetched', function () {
-        shipit.start('deploy:build');
-    });
+    utils.registerTask(shipit, 'deploy', [
+        'deploy:init',
+        'deploy:fetch',
+        'deploy:build',
+        'deploy:update',
+        'deploy:publish',
+        'deploy:clean',
+        'deploy:finish'
+    ]);
 };
--- task/deploy/build.js
+++ task/deploy/build.js
@@ -3,18 +3,26 @@ var utils = require('shipit-utils');
 var chalk = require('chalk');
 var util = require('util');
 var path = require('path');
+var _ = require('lodash');
 
 function build(gruntOrShipit) {
     utils.registerTask(gruntOrShipit, 'deploy:build', task);
 
     function task() {
         var shipit = utils.getShipit(gruntOrShipit);
+        var xbuildOptions = {
+            framework: 'v4.0',
+            solutionDir: '',
+            configuration: 'release'
+        };
+        _.assign(xbuildOptions, shipit.config.xbuild);
+
         return nugetRestore().then(buildProject);
 
         function nugetRestore() {
             shipit.log('nuget restore "%s"', shipit.config.workspace);
             var exec_command = util.format('nuget restore');
-            var cwd = path.resolve(shipit.config.workspace, shipit.config.xbuild.solutionDir);
+            var cwd = path.resolve(shipit.config.workspace, xbuildOptions.solutionDir);
             return shipit.local(exec_command, {cwd: cwd}).then(function () {
                 shipit.log(chalk.green('nuget restore success.'));
                 shipit.emit('builded');
@@ -23,15 +31,15 @@ function build(gruntOrShipit) {
 
         function buildProject() {
             shipit.log('begin mono xbuild repository in "%s"', shipit.config.workspace);
-            var cwd = path.resolve(shipit.config.workspace, shipit.config.xbuild.solutionDir);
+            var cwd = path.resolve(shipit.config.workspace, xbuildOptions.solutionDir);
             var exec_command = [
                 "xbuild",
-                shipit.config.xbuild.csprojPath,
-                "/p:configuration=" + shipit.config.xbuild.configuration,
-                "/p:TargetFrameworkVersion=" + shipit.config.xbuild.framework
+                xbuildOptions.csprojPath,
+                "/p:configuration=" + xbuildOptions.configuration,
+                "/p:TargetFrameworkVersion=" + xbuildOptions.framework
             ];
-            for (var p in shipit.config.xbuild.properties) {
-                exec_command.push(["/p:", p, "=", shipit.config.xbuild.properties[p]].join(""));
+            for (var p in xbuildOptions.properties) {
+                exec_command.push(["/p:", p, "=", xbuildOptions.properties[p]].join(""));
             }
             exec_command = exec_command.join(" ");
 
